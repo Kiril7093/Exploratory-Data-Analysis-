8241c52038b419e6161a725e0a15c750484b9ca0
Kiril <klh7093@gmail.com>
2017-02-14 19:13:24 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,16 @@
+#Creating a working directory:
+if(!file.exists("./data")){dir.create("./data")}
+
+#Unzip "exdata_data_household_power_consumption" into working directory:
+unzip(zipfile="./data/exdata_data_household_power_consumption.zip",exdir="./data")
+
+#Reading the table:
+householdData <- "./data/household_power_consumption.txt"
+data <- read.table(householdData, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+relevantData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot data:
+powerData <- as.numeric(relevantData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(powerData, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
